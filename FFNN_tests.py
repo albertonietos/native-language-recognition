b23382e49d7afc5d676e68d2ab5602147ca9c962
Alberto <ansnieto@gmail.com>
2020-12-08 18:48:55 +0200

    Fix criterion for loss function

--- FFNN_tests.py
+++ FFNN_tests.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.metrics import recall_score
 
 # %% Import data
@@ -18,18 +18,15 @@ dev = pd.read_csv(os.path.join(folder_path + "devel.csv"))
 
 # %% Format data
 # Separate into features (X) and labels (y)
-X_train, y_train = train.iloc[:, 1:], train.loc[:, ["l1_nationality"]]
-X_dev, y_dev = dev.iloc[:, 1:], dev.loc[:, ["l1_nationality"]]
+X_train, y_train = train.iloc[:, 1:], train["l1_nationality"]
+X_dev, y_dev = dev.iloc[:, 1:], dev["l1_nationality"]
 
 # delete unneeded data
 del train, dev
 
 # %% Build pipeline
-# feat_pipeline = Pipeline('std_scaler', StandardScaler())
-# label_pipeline = Pipeline(['label_encoder', LabelEncoder()])
 scaler = StandardScaler().fit(X_train)
-encoder = OneHotEncoder(sparse=False).fit(y_train)
-# encoder = LabelEncoder().fit(y_train)
+encoder = LabelEncoder().fit(y_train)
 
 X_train, y_train = scaler.transform(X_train), encoder.transform(y_train)
 X_dev, y_dev = scaler.transform(X_dev), encoder.transform(y_dev)
@@ -37,14 +34,15 @@ X_dev, y_dev = scaler.transform(X_dev), encoder.transform(y_dev)
 # %% Build MLP
 device = torch.device("cuda:0")
 n_inputs = X_train.shape[1]
-n_outputs = y_train.shape[1]
-X_train, y_train = torch.Tensor(X_train), torch.Tensor(y_train)
-X_dev, y_dev = torch.Tensor(X_dev), torch.Tensor(y_dev)
+n_outputs = len(np.unique(y_train))
+
+X_train, y_train = torch.Tensor(X_train), torch.from_numpy(y_train)
+X_dev, y_dev = torch.Tensor(X_dev), torch.from_numpy(y_dev)
 
 
 # %%
 class MLP(nn.Module):
-    def __init__(self, n_inputs=n_inputs):
+    def __init__(self, n_inputs=n_inputs, n_outputs=n_outputs):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(n_inputs, 200)
         self.fc2 = nn.Linear(200, 100)
@@ -60,7 +58,7 @@ def compute_uar(model, inputs, targets):
     with torch.no_grad():
         inputs, targets = inputs.to(device), targets.to(device)
         outputs = model.forward(inputs)
-        uar = recall_score(np.argmax(targets.cpu().numpy(), axis=1), np.argmax(outputs.cpu().numpy(), axis=1),
+        uar = recall_score(targets.cpu().numpy(), np.argmax(outputs.cpu().numpy(), axis=1),
                            average='macro')
 
         return uar
@@ -75,23 +73,23 @@ def train_model(X_train, y_train, X_dev, y_dev, l2_lambda=l2_lambda):
 
     train_uar = []
     dev_uar = []
-    X_train, y_train = X_train.to(device), y_train.to(device)
-    X_dev, y_dev = X_dev.to(device), y_dev.to(device)
+    X_train, y_train = X_train.to(device), y_train.to(device, dtype=torch.int64)
+    X_dev, y_dev = X_dev.to(device), y_dev.to(device, dtype=torch.int64)
     opt = torch.optim.Adam(model.parameters())
+    criterion = nn.CrossEntropyLoss()
 
     for i in range(200):
         model.zero_grad()
         out = model(X_train)
-        loss = nn.CrossEntropyLoss(out, y_train)
+        loss = criterion(out, y_train)
         # Apply regularization
         l2_norm = sum(p.pow(2.0).sum() for p in model.parameters())
         loss = loss + l2_lambda * l2_norm
         loss.backward()
-        if i % 100:
+        if i % 10:
             trn, dev = compute_uar(model, X_train, y_train), compute_uar(model, X_dev, y_dev)
             train_uar.append(trn)
             dev_uar.append(dev)
-            # print(f"train_uar={trn}, dev_uar={dev}")
         opt.step()
 
     return train_uar, dev_uar
@@ -108,6 +106,7 @@ plt.show()
 
 # %% Grid Search L2 lambda
 lmbd = [0.000001, 0.00001, 0.0001, 0.001, 0.01]
+lmbd = [0.001, 0.001*3, 0.001*9]
 train_list, dev_list = [], []
 for l2 in lmbd:
     train_uar, dev_uar = train_model(X_train, y_train, X_dev, y_dev, l2_lambda=l2)
